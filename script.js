7780a0a0e253be8146e6b9d7755726f72569893f
Navdeshsinghsaini <naveshsinghsaini@gmail.com>
2023-02-07 22:25:07 +0530

    initial4

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
+    str.trim();
+    let arr = str.split("");
+    // console.log(arr);
+     let k = arr.reverse();
+    // console.log(arr);
+    if(k == arr ){
+        return true
+    }else {return false}
+
 
 }
-module.exports = palindrome
+// module.exports = palindrome
