4b584a8a605eb3a4bc76a7111f862d89eb88938f
Navdesh Singh Saini <120104452+Navdeshsinghsaini@users.noreply.github.com>
2023-03-03 00:52:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // complete the given function
 
 function palindrome(str){
-    str.trim();
-    let arr = str.split("");
+   let m = str.toLowerCase();
+    m.trim();
+    let arr = m.split("");
     // console.log(arr);
      let k = arr.reverse();
     // console.log(arr);
